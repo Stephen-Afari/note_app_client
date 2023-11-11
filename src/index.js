8ff5bfde8202dcf0353d261d58d50e2ff9093cc9
Stephen Afari <demorganafari@yahoo.com>
2023-11-11 05:04:41 +0000

    preparing app for deployment

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+//disabling React devtools for deployment in our application
+if(process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
